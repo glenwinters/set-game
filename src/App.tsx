b4362515a5153110cbec080d171a31bba8d8fdc8
Glen Winters <glenwinters@gmail.com>
2020-09-26 20:58:53 -0500

    feat: create 3x4 card grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
-function App() {
+// TODO: Make the root full screen so the background-color is everywhere and
+// then center the Set board
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      backgroundColor: "#e9e9e9"
+    }
+  })
+);
+
+// TODO: Move SetCard to its own file and give it props
+const SetCard: React.FC = () => {
+ return (
+  <Card>
+    <CardContent>
+      <Typography align="center">~ ~ ~</Typography>
+    </CardContent>
+  </Card>
+ );
+}
+
+// TODO: How will we map an array of things to this grid since the 12 items are
+// broken up by Grid containers?
+const App: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <SetCard />
+        </Grid>
+        <Grid item xs>
+          <SetCard />
+        </Grid>
+        <Grid item xs>
+          <SetCard />
+        </Grid>
+        <Grid item xs>
+          <SetCard />
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <SetCard />
+        </Grid>
+        <Grid item xs>
+          <SetCard />
+        </Grid>
+        <Grid item xs>
+          <SetCard />
+        </Grid>
+        <Grid item xs>
+          <SetCard />
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <SetCard />
+        </Grid>
+        <Grid item xs>
+          <SetCard />
+        </Grid>
+        <Grid item xs>
+          <SetCard />
+        </Grid>
+        <Grid item xs>
+          <SetCard />
+        </Grid>
+      </Grid>
     </div>
   );
-}
+};
 
 export default App;
